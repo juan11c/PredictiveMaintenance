dced7cbab2cb59868afe6a814741f250f2bc2ba7
Juan <juan.cabello1@outlook.com>
2026-02-01 20:01:42 -0600

    feat: add SensorData controller and API endpoints

--- PredictiveMaintenance.Api/Program.cs
+++ PredictiveMaintenance.Api/Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<ITelemetryService, TelemetryService>();
+//builder.Services.AddScoped<ITelemetryService, TelemetryService>();
 
 // Add controllers
 builder.Services.AddControllers();
