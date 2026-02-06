b12f10e9bd0d50b6768492d9a79cdcf30f56040d
Juan <juan.cabello1@outlook.com>
2026-02-06 16:04:16 -0600

    feat: add Sensor entity and EF Core configuration

---
+++ PredictiveMaintenance.Infrastructure/Persistence/Configurations/SensorConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PredictiveMaintenance.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PredictiveMaintenance.Infrastructure.Persistence.Configurations
+{
+    public class SensorConfiguration : IEntityTypeConfiguration<Sensor>
+    {
+        public void Configure(EntityTypeBuilder<Sensor> builder)
+        {
+            // Primary Key
+            builder.HasKey(m => m.Id);
+
+            builder.Property(m => m.MachineId)
+                   .IsRequired()
+                   .HasMaxLength(50);
+
+            builder.Property(m => m.Type)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            builder.Property(m => m.Unit)
+                .IsRequired();
+        }
+    }
+}
